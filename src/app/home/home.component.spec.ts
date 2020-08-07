d0aca6f957f696d7306d213581c1fd7002cc1422
Mayank Pathak <mayankkv1@gmail.com>
2020-08-07 15:17:51 +0530

    Fixes done

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,14 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { SpacexService } from '../services/spacex.service';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from "@angular/router/testing";
+
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let testBedSpacexService: SpacexService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      imports: [HttpClientModule,RouterTestingModule],
+      providers: [SpacexService]
     })
     .compileComponents();
   }));
@@ -17,9 +24,10 @@ describe('HomeComponent', () => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    testBedSpacexService = TestBed.get(SpacexService)
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should check the spacex service', () => {
+    expect(testBedSpacexService instanceof SpacexService).toBeTruthy();
   });
 });
---
+++ src/app/services/spacex.service.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { SpacexService } from './spacex.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+
+describe('SpacexService', () => {
+  let spacexService: SpacexService;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule],
+        providers: [SpacexService]
+    });
+    spacexService = TestBed.inject(SpacexService);
+  });
+
+  beforeEach(() => {
+    spacexService = TestBed.get(SpacexService)
+    httpTestingController = TestBed.get(HttpTestingController)
+  });
+
+  it('should test service', () => {
+    expect(spacexService).toBeTruthy();
+  })
+
+  it('should test HttpClient.get', () => {
+    spacexService.getLaunces("?limit=100").subscribe((data)=>{
+        expect(data).not.toBeNull();
+    }) 
+  });
+});
